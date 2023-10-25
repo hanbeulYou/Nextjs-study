267389fbe22233c4248bb916299af0ede5a9b9c8
hanbeulYou <hanbeul.you@gmail.com>
2023-10-25 22:43:17 +0900

    lecture-3 feature: json과 swr을 활용해 매장 데이터 호출 및 관리

--- lecture-3/pages/index.tsx
+++ lecture-3/pages/index.tsx
@@ -1,12 +1,25 @@
-import { Fragment } from 'react';
-import Header from '../components/common/Header';
-import styles from '../styles/header.module.scss';
+import { Fragment, useEffect } from 'react';
 import Link from 'next/link';
+import { NextPage } from 'next';
 import { VscFeedback } from 'react-icons/vsc';
 import { AiOutlineShareAlt } from 'react-icons/ai';
+import Header from '../components/common/Header';
+import styles from '../styles/header.module.scss';
 import MapSection from '../components/home/MapSection';
+import { Store } from '../types/store';
+import useStores from '../hooks/useStores';
 
-export default function Home() {
+interface Props {
+  stores: Store[];
+}
+
+const Home: NextPage<Props> = ({ stores }) => {
+  const { initializeStores } = useStores();
+
+  useEffect(() => {
+    initializeStores(stores);
+  }, [initializeStores, stores]);
+
   return (
     <Fragment>
       <Header
@@ -26,10 +39,20 @@ export default function Home() {
           </Link>,
         ]}
       />
-
       <main style={{ width: '100%', height: '100%' }}>
         <MapSection />
       </main>
     </Fragment>
   );
+};
+export default Home;
+
+export async function getStaticProps() {
+  /** TODO: next api routes로 불러오기 */
+  const stores = (await import('../public/stores.json')).default;
+
+  return {
+    props: { stores },
+    revalidate: 60 * 60,
+  };
 }
