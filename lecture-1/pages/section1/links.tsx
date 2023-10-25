4aeee2c1d7f907f472bc9fdea7dd473914d7a5eb
hanbeulYou <hanbeul.you@gmail.com>
2023-10-25 20:39:44 +0900

    lecture-1 feature: useRouter Hook

--- lecture-1/pages/section1/links.tsx
+++ lecture-1/pages/section1/links.tsx
@@ -1,17 +1,31 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function Links() {
+  const router = useRouter();
+  useEffect(() => {
+    router.prefetch('/section1/getStaticProps');
+  }, [router]);
+
   return (
     <main>
       <h1>Links</h1>
-      <div style={{ height: '200vh' }} />
+      <button
+        onClick={() => {
+          router.push('/section1/getStaticProps');
+        }}
+      >
+        /getStaticProps
+      </button>
+      {/* <div style={{ height: '200vh' }} /> */}
 
       {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
       {/*<a href="/section1/getStaticProps">/getStaticProps</a>*/}
 
-      <Link href="/section1/getStaticProps" style={{ color: 'red' }}>
+      {/* <Link href="/section1/getStaticProps" style={{ color: 'red' }}>
         /getStaticProps
-      </Link>
+      </Link> */}
       {/** https://github.com/vercel/next.js/blob/canary/packages/next/client/link.tsx#L487 */}
     </main>
   );
