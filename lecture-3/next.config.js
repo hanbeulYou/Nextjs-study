c625ba177be09bb91da970d53c08a4969f9dff2d
hanbeulYou <hanbeul.you@gmail.com>
2023-10-25 22:36:25 +0900

    Revert "Revert "lecture-3 init: next/script를 활용해 nmap 호출""
    This reverts commit 6117015978e44b75c2901270ad5dd393f40f0e0e.

--- lecture-3/next.config.js
+++ lecture-3/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // strickMode의 경우 dev 상태에서 useEffect가 2번씩 실행됨 -> Map.tsx에서 destroy 당함
   reactStrictMode: true,
   images: {
     domains: ['lecture-1.vercel.app'],
