757c2860d835876522590ff89b52dbe170597c9d
hanbeulYou <hanbeul.you@gmail.com>
2023-11-05 21:20:43 +0900

    lecture-6 init: 기존 프로젝트 코드 생성

---
+++ lecture-6/pages/index.tsx
@@ -0,0 +1,52 @@
+import { Fragment, useEffect } from 'react';
+import { NextPage } from 'next';
+import Header from '../components/home/Header';
+import MapSection from '../components/home/MapSection';
+import DetailSection from '../components/home/DetailSection';
+import { Store } from '../types/store';
+import useStores from '../hooks/useStores';
+import { NextSeo } from 'next-seo';
+
+interface Props {
+  stores: Store[];
+}
+
+const Home: NextPage<Props> = ({ stores }) => {
+  const { initializeStores } = useStores();
+
+  useEffect(() => {
+    initializeStores(stores);
+  }, [initializeStores, stores]);
+
+  return (
+    <Fragment>
+      <NextSeo
+        title="매장 지도"
+        description="Next.js 시작하기 강의를 위한 매장 지도 서비스."
+      />
+      <Header />
+      <main
+        style={{
+          position: 'relative',
+          width: '100%',
+          height: '100%',
+          overflow: 'hidden',
+        }}
+      >
+        <MapSection />
+        <DetailSection />
+      </main>
+    </Fragment>
+  );
+};
+export default Home;
+
+export async function getStaticProps() {
+  /** TODO: next api routes로 불러오기 */
+  const stores = (await import('../public/stores.json')).default;
+
+  return {
+    props: { stores },
+    revalidate: 60 * 60,
+  };
+}
