7f244142ba5ef201efc1b47a17daa96b7643b717
hanbeulYou <hanbeul.you@gmail.com>
2023-11-14 15:39:38 +0900

    lecture-7 feature: feedback page에 firebase 연동

--- lecture-5/pages/feedback.tsx
+++ lecture-5/pages/feedback.tsx
@@ -1,17 +1,45 @@
 import { Fragment } from 'react';
-import Header from '../components/common/Header';
+import { GetServerSideProps, NextPage } from 'next';
 import { NextSeo } from 'next-seo';
+import Header from '../components/common/Header';
+import FeedbackSection from '../components/feedback/FeedbackSection';
+import type { Feedback } from '../types/feedback';
+import { getFeedbackListFromFirestore } from '../firebase/feedback';
 
-export default function Feedback() {
+interface Props {
+  initialFeedbackList: Feedback[];
+}
+
+export const FeedbackPage: NextPage<Props> = ({ initialFeedbackList }) => {
   return (
     <Fragment>
       <NextSeo
         title="피드백"
-        description="매장 지도 서비스에 대한 피드백"
-        canonical="https://nextjs-study-zeta.vercel.app/feedback"
+        description="매장 지도 서비스에 대한 피드백을 받습니다."
+        canonical="https://inflearn-nextjs.vercel.app/feedback"
       />
       <Header />
-      <main></main>
+
+      <main
+        style={{
+          position: 'relative',
+          width: '100%',
+          height: '100%',
+          overflow: 'hidden',
+          touchAction: 'pinch-zoom',
+        }}
+      >
+        <FeedbackSection initialFeedbackList={initialFeedbackList} />
+      </main>
     </Fragment>
   );
-}
+};
+export default FeedbackPage;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {
+      initialFeedbackList: await getFeedbackListFromFirestore(),
+    },
+  };
+};
